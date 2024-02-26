263d06c4c1f7f00c3a5cfbfe1d1ad575e97a56d9
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2024-02-26 11:16:01 +0500

    done

--- base/views.py
+++ base/views.py
@@ -1,16 +1,16 @@
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.views.generic import FormView
-from django.contrib.auth.views import LoginView, LogoutView
+import os
+from django.conf import settings
 from django.views.generic import TemplateView
 from django.urls import reverse_lazy
-
+from .stt import stt
+from .models import *
 class HomeView(TemplateView):
     template_name = "pages/home.html"
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context["company_name"] = "My Awesome Company"
-    #     context["description"] = "We provide amazing products and services."
-    #     return context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["text"] = stt(File.objects.get().file)
+        context["files"] = File.objects.all()
+        return context
 
     
