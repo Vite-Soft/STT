8ca4a19f1cabc9b3703527afda62e06f3443114d
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2024-03-03 00:01:52 +0500

    done

--- base/views.py
+++ base/views.py
@@ -14,23 +14,17 @@ class HomeView(TemplateView):
         return context
     
 
-class FileListView(ListView):
-    model = File
+class FileListView(TemplateView):
     template_name = "pages/file.html"
-    paginate_by = 100  # if pagination is desired
     
     def get_context_data(self, **kwargs):
-        # TEXT = (1,)
         context = super().get_context_data(**kwargs)
-        d = []
+        data = []
         for i in File.objects.all():
             temp = {
                 'url': i.file.url,
                 'text': stt(i.file)
             }
-            d.append(temp)
-            # context["text"] = stt(i.file)
-        context['data'] = d
+            data.append(temp)
+        context['data'] = data
         return context
-        # context["data"] = d
-        # return context
