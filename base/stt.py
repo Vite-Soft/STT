007026f447e62346fd5cc340877938665946b859
shakhzodtojiev <tojiyevshakhzod@gmail.com>
2024-03-21 07:23:42 +0500

    done

--- base/stt.py
+++ base/stt.py
@@ -12,17 +12,17 @@ if not os.path.exists(r"base/vosk-model-small-uz"):
     exit(1)
 
 def stt(wav_file):
-  if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
-    print("Audio file must be WAV format mono PCM.")
-    sys.exit(1)
-    
   # Open the wave file
   with wave.open(wav_file, "rb") as wf:
-  # Load the Vosk model 
-   model = vosk.Model(r"base/vosk-model-small-uz")
+    if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+        print("Audio file must be WAV format mono PCM.")
+        sys.exit(1)
 
-   # Create a KaldiRecognizer
-   rec = vosk.KaldiRecognizer(model, wf.getframerate())
+    # Load the Vosk model 
+    model = vosk.Model(r"base/vosk-model-small-uz")
+
+    # Create a KaldiRecognizer
+    rec = vosk.KaldiRecognizer(model, wf.getframerate())
 
   while True:
         data = wf.readframes(wf.getnframes()) # Process audio in chunks
